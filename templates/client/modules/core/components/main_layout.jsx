505a0e4997ea5aa99156eea89f7ce3c632894159
Todd Hancock <thancock20@hotmail.com>
2016-09-09 12:57:19 -0500

    Update main_layout.jsx to load global styles and add title using react-helmet

--- templates/client/modules/core/components/main_layout.jsx
+++ templates/client/modules/core/components/main_layout.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+
+import styles from '../../../styles/index.js';
+import load from '../../../../lib/style_loader';
+load(styles);
 
 const Layout = ({content = () => null }) => (
   <div>
+    <Helmet
+      defaultTitle="<%= appName %>"
+      titleTemplate="<%= appName %> - %s"
+    />
     <div>
       {content()}
     </div>
