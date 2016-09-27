e7cebac6003533551402f6ffa07e32d5605eda3a
Todd Hancock <thancock20@hotmail.com>
2016-09-27 15:46:30 -0500

    Remove dependency on js-yaml

--- test/utils_test.js
+++ test/utils_test.js
@@ -1,6 +1,5 @@
 import {expect} from 'chai';
 import fse from 'fs-extra';
 import _ from 'lodash';
-import yaml from 'js-yaml';
 
 import {setupTestApp, teardownTestApp, checkFileOrDirExists} from './test_helpers';
