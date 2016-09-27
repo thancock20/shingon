78a4e43cc7c0892889d128753a22cf0d010b4633
Todd Hancock <thancock20@hotmail.com>
2016-09-26 22:16:46 -0500

    Add global styling to storybook config

--- templates/.storybook/config.js
+++ templates/.storybook/config.js
@@ -3,6 +3,10 @@ import { disable } from 'react-komposer';
 
 disable();
 
+import styles from '../client/styles/index';
+import load from '../lib/style_loader';
+load(styles);
+
 function loadStories() {
   // require as many as stories you need.
   require('../client/modules/core/components/.stories/index.js');
