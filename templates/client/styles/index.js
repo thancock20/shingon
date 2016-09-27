6969403603cd724008c5f0c3eaf44988707532a3
Todd Hancock <thancock20@hotmail.com>
2016-09-26 22:14:20 -0500

    Add global styling and normalize

---
+++ templates/client/styles/index.js
@@ -0,0 +1,6 @@
+import objectMerge from 'object-merge';
+import normalize from './normalize';
+
+export default objectMerge(
+  normalize,
+);
--- test/commands/create_test.js
+++ test/commands/create_test.js
@@ -32,6 +32,8 @@ describe("create command", function() {
     expect(checkFileOrDirExists('./blog/client/modules/core/index.js')).to.equal(true);
     expect(checkFileOrDirExists('./blog/client/modules/core/routes.jsx')).to.equal(true);
     expect(checkFileOrDirExists('./blog/client/modules/core/libs/')).to.equal(true);
+    expect(checkFileOrDirExists('./blog/client/styles/index.js')).to.equal(true);
+    expect(checkFileOrDirExists('./blog/client/styles/normalize.js')).to.equal(true);
     expect(checkFileOrDirExists('./blog/client/main.js')).to.equal(true);
     expect(checkFileOrDirExists('./blog/lib/collections/index.js')).to.equal(true);
     expect(checkFileOrDirExists('./blog/lib/style_loader.js')).to.equal(true);
